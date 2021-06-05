7debb232a861c910af99f58f45fc5eca3e0a1da4
khaledrefaat <khaledrefaat08@gmail.com>
2021-06-06 01:23:37 +0200

    pug template engine files done

---
+++ Max Projects/test/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const routerAdmin = require('./routes/admin');
+const routerShop = require('./routes/shop');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', routerAdmin.routes);
+app.use(routerShop);
+app.use((req, res, next) => {
+  res.status(404).render('404', { docTitle: 'Page Not Found' });
+});
+
+app.listen(9000);
