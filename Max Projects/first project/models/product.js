aee793f4609cde4bea6f00850bbdd12a455c8843
khaledrefaat <khaledrefaat08@gmail.com>
2021-06-08 21:16:18 +0200

    mvc pattern refactor

--- Max Projects/first project/models/product.js
+++ Max Projects/first project/models/product.js
@@ -1,38 +1,36 @@
 const path = require('path');
 const fs = require('fs');
 
+const p = path.join(
+  path.dirname(require.main.filename),
+  'data',
+  'products.json'
+);
+
+const getProductFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) return cb([]);
+    cb(JSON.parse(fileContent));
+  });
+};
+
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
 
   save() {
-    const p = path.join(
-      path.dirname(require.main.filename),
-      'data',
-      'products.json'
-    );
     fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
+      getProductFromFile(products => {
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
       });
     });
   }
 
   static fetchAll(cb) {
-    const p = path.join(
-      path.dirname(require.main.filename),
-      'data',
-      'products.json'
-    );
-    fs.readFile(p, (err, fileContent) => {
-      if (err) cb([]);
-      cb(JSON.parse(fileContent));
-    });
+    getProductFromFile(cb);
   }
 };
