4e33d8e972efbe5f9e23859620d5ed8bc5fead29
khaledrefaat <khaledrefaat08@gmail.com>
2021-11-24 20:19:11 +0200

    crypting password when signingup and comparing it when loging in

--- Max Projects/Shop/controllers/auth.js
+++ Max Projects/Shop/controllers/auth.js
@@ -1,29 +1,37 @@
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     docTitle: 'Login',
     path: '/login',
-    isAuthenticated: req.session.isLoggedin,
+    isAuthenticated: false,
   });
 };
 
-exports.postLogin = async (req, res, next) => {
+exports.postLogin = (req, res, next) => {
   const { email, password } = req.body;
 
-  const user = await User.findOne({ email: email });
-
-  if (user && user.password === password) {
-    req.session.isLoggedin = true;
-    req.session.user = user;
-    req.session.save(err => {
+  return User.findOne({ email })
+    .then(user => {
+      return bcrypt.compare(password, user.password).then(result => {
+        if (user && result) {
+          req.session.isLoggedin = true;
+          req.session.user = user;
+          return req.session.save(err => {
+            console.log(err);
+            res.redirect('/');
+          });
+        }
+        req.isLoggedin = false;
+        return res.redirect('/login');
+      });
+    })
+    .catch(err => {
       console.log(err);
-      res.redirect('/');
+      return res.redirect('/login');
     });
-  } else {
-    req.isLoggedin = false;
-    res.redirect('/login');
-  }
 };
 
 exports.postLogout = (req, res, next) => {
@@ -36,19 +44,44 @@ exports.getSignup = (req, res, next) => {
   res.render('auth/signup', {
     docTitle: 'Sign Up',
     path: '/signup',
-    isAuthenticated: req.session.isLoggedin,
+    isAuthenticated: false,
   });
 };
 
 exports.postSignUp = (req, res, next) => {
-  const { name, email, password } = req.body;
+  const { name, email, password, confirmPassword } = req.body;
 
-  const user = new User({ email, password, name, cart: { items: [] } });
-  user
-    .save()
-    .then(() => {
-      console.log('Created User Complete ^_^');
-      res.redirect('/');
+  User.findOne({ email: email })
+    .then(user => {
+      if (password !== confirmPassword || user) {
+        return res.redirect('/signup');
+      }
+
+      return bcrypt
+        .hash(password, 12)
+        .then(hashedPassword => {
+          const user = new User({
+            email,
+            password: hashedPassword,
+            name,
+            cart: { items: [] },
+          });
+          return user
+            .save()
+            .then(() => {
+              req.session.isLoggedin = true;
+              req.session.user = user;
+              req.session.save(err => {
+                console.log(err);
+                return res.redirect('/signup');
+              });
+            })
+            .catch(err => {
+              console.log(err);
+              return res.redirect('/signup');
+            });
+        })
+        .catch(err => console.log(err));
     })
     .catch(err => console.log(err));
 };
