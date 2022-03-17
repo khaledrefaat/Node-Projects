2eee0f0ab83e8a942cdd1cbe9ba4ae9c9631b2d0
khaledrefaat <khaledrefaat08@gmail.com>
2022-03-17 02:45:01 +0200

    starter files

--- Max Projects/rest/routes/feed.js
+++ Max Projects/rest/routes/feed.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const HttpError = require('../models/http-error');
+const { body } = require('express-validator');
 
 const router = express.Router();
 
 const { getPosts, postPosts } = require('../controllers/feed');
 
 router.get('/', getPosts);
-router.post('/', postPosts);
+router.post(
+  '/',
+  [
+    body('title', 'Please enter title with at least 3 chars.')
+      .trim()
+      .isLength({ min: 7 }),
+    body('content', 'Please enter description with at least 10 chars.')
+      .trim()
+      .isLength({ min: 5 }),
+  ],
+  postPosts
+);
 
 module.exports = router;
